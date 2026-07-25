b75888db663d21d5a04ea150266983296c3e76ce
ibrahimkhanmohammad <ibrahimfirdousmd@gmail.com>
2026-07-25 06:38:44 +0530

    introduction to builtin functions in a list

---
+++ 015_lists/2_built_in_func.py
@@ -0,0 +1,28 @@
+marks = [41, 24, 85, 96, 36, 75]
+print(marks)
+
+#   len() - to find the length of the list
+size = len(marks)
+print(size)
+
+#   sum() - to find the sum of the elements in a list
+total = sum(marks)
+print(total)
+
+#   min() - to find minimum element in a list
+mini = min(marks)
+print(mini)
+
+#   max() - to find maximum element in a list
+maxi = max(marks)
+print(maxi)
+
+#   sorted() - returns the new list, but does not affect the original list, i.e; original list remains unchanged, by sorting it in ascending order
+asc_sorted = sorted(marks)
+print(asc_sorted)
+print(marks)
+
+#   for descending sorted order list
+desc_sorted = sorted(marks, reverse=True)
+print(desc_sorted)
+print(marks)
