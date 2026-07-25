4f8847b2b07e0f8a2eeb0aed8393faf8d2a9b68e
ibrahimkhanmohammad <ibrahimfirdousmd@gmail.com>
2026-07-25 07:34:27 +0530

    indexing and updating in lists

---
+++ 015_lists/3_indexing.py
@@ -0,0 +1,8 @@
+lst = ['Ibrahim', 19, 6.12, True, 'Chennai']
+print(lst)
+
+print(lst[0])   # first element +ve indexing
+print(lst[4])   # last element +ve indexing
+print(lst[-1])  # first element  -ve indexing
+print(lst[-5])  # last element -ve indexing
+
---
+++ 015_lists/4_updating.py
@@ -0,0 +1,5 @@
+lst = ['Ibrahim', 19, 6.12, True, 'Chennai']
+print(lst)
+
+lst[2] = 'Bhimavaram'
+print(lst)  #updates the index 1 element of lst
