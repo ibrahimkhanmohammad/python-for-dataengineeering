7a1ce0a1272bf4ee56b51409f7a6c865e74c1e54
ibrahimkhanmohammad <ibrahimfirdousmd@gmail.com>
2026-07-25 07:51:41 +0530

    wrapping up indexing and updating in lists

---
+++ 015_lists/5_practise_problem1.py
@@ -0,0 +1,12 @@
+anime = ['attack on titan', 'one piece', 'bleach', 'erased']
+print(anime)
+
+print(anime[0])
+print(anime[3])
+print(anime[-1])
+print(anime[-4])
+
+n = len(anime)
+
+print(anime[n-1])
+print(anime[-n])
---
+++ 015_lists/6_practise_problem2.py
@@ -0,0 +1,6 @@
+marks = [44, 19, 18, 100, 98]
+print(marks)
+
+marks[1] = 0
+marks[3] = 0
+print(marks)
