93c236f6d1fe560e2c1f1bb3694a38cd4f98da8c
ibrahimkhanmohammad <ibrahimfirdousmd@gmail.com>
2026-07-26 11:32:44 +0530

    list slicing operations

---
+++ 015_lists/7_slicing.py
@@ -0,0 +1,27 @@
+marks = [13, 12, 11, 20, 21, 24]
+print(marks)
+
+updated_marks = marks[1:5]
+print(updated_marks)
+updated_marks = marks[1:3]
+print(updated_marks)
+updated_marks = marks[1:]
+print(updated_marks)
+updated_marks = marks[:6]
+print(updated_marks)
+updated_marks = marks[:]
+print(updated_marks)
+updated_marks = marks[1:6:2]
+print(updated_marks)
+updated_marks = marks[::4]
+print(updated_marks)
+updated_marks = marks[4:1:-1]
+print(updated_marks)
+updated_marks = marks[4::-1]
+print(updated_marks)
+updated_marks = marks[:1:-2]
+print(updated_marks)
+updated_marks = marks[::-1]
+print(updated_marks)
+
+print(marks)
