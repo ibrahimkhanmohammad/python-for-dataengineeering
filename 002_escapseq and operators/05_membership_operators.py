be648f7f32d9375d3a8a7f7fcbf321e4b2169ff6
ibrahimkhanmohammad <ibrahimfirdousmd@gmail.com>
2026-08-02 22:24:28 +0530

    added membership operators after lists

---
+++ 002_escapseq and operators/05_membership_operators.py
@@ -0,0 +1,11 @@
+#   membership operators -> in , not in (return boolean value)
+
+nums = [10, 20, 30, 40, 50]
+
+#   in:  print True if element does, exists in a list if not False
+print(20 in nums)
+print(100 in nums)
+
+#   not in:  print True if element does not, exists in a list if exists False
+print(10 not in nums)
+print(100 not in nums)
