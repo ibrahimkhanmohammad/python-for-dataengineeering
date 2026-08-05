6a1dde232288a4218f5a89affc6a8a3cd1e9de5b
ibrahimkhanmohammad <ibrahimfirdousmd@gmail.com>
2026-08-05 16:55:45 +0530

    methods in tuples

---
+++ 016_tuples/6_methods in tuples/1_tuple_methods.py
@@ -0,0 +1,9 @@
+#   as tuples does not support the updation so there are only 2 built-in methods we can use
+nums = (24,21,1,20,13,24,64)
+#   count()
+print(nums.count(24))
+print(nums.count(13))
+
+#   index()
+print(nums.index(24))
+print(nums.index(13))
