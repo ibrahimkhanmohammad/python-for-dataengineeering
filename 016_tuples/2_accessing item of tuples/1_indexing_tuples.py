79ba76fa07c3a737c470cacb40824ea84aa5626b
ibrahimkhanmohammad <ibrahimfirdousmd@gmail.com>
2026-08-05 16:52:28 +0530

    accessing items in tuples through indexing and slicing

---
+++ 016_tuples/2_accessing item of tuples/1_indexing_tuples.py
@@ -0,0 +1,6 @@
+#   accessing elements of tuple by using indexing
+details = (101, 'Asta', 5.4, True)
+print(details[0])
+print(details[3])
+print(details[-2])
+print(details[-3])
---
+++ 016_tuples/2_accessing item of tuples/2_slicing_tuples.py
@@ -0,0 +1,6 @@
+#   slicing portions of tuple by using slicing
+details = (101, 'Asta', 5.4, True)
+print(details[0:])
+print(details[:3])
+print(details[::2])
+print(details[::-1])
