a3716f99fe3dcf9a9168d1c2438344a79ce5bdd7
ibrahimkhanmohammad <ibrahimfirdousmd@gmail.com>
2026-08-08 13:51:24 +0530

    basics of dictionaries

---
+++ 017_dictionaries/1_basic of dictionaries/creation_of_dictionary.py
@@ -0,0 +1,13 @@
+#   creating an empty dictionary
+my_dict = {}
+print(type(my_dict))
+
+#   creation of dictionary with key-value pairs
+my_dictionary =  {'name': 'ibrahim',
+                  'age': 20,
+                  'city': 'bhimavaram',
+                  'is_active': True
+                  }
+print(my_dictionary)
+print(len(my_dictionary))
+print(type(my_dictionary))
