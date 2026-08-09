91fe474c794406c7d44ed2e2a6dd640101ad8483
ibrahimkhanmohammad <ibrahimfirdousmd@gmail.com>
2026-08-09 23:08:34 +0530

    nested dictionaries list in dict

---
+++ 017_dictionaries/8_nested dictionaries part 2/1_accessing.py
@@ -0,0 +1,13 @@
+student = {
+"name": "Rahul",
+"age": 21,
+"subjects": ["Math", "Science", "English"],
+"marks": [85, 92, 78]
+}
+
+print(student)
+# print(student['name'])
+# print(student['name'], student['subjects'])
+# print(student['subjects'][0])
+# print(student['subjects'][-1])
+# print(student['marks'][1])
