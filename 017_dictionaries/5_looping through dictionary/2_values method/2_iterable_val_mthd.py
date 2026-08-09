aa7e1f85281d655462e8be66eff0e3d5b7098a2f
ibrahimkhanmohammad <ibrahimfirdousmd@gmail.com>
2026-08-09 10:55:47 +0530

    values method

---
+++ 017_dictionaries/5_looping through dictionary/2_values method/2_iterable_val_mthd.py
@@ -0,0 +1,10 @@
+#   we can iterate by using for loop so that we get values, such that
+marks = {
+    'science': 40,
+    'maths': 80,
+    'physics': 65,
+    'chemistry': 50
+}
+
+for val in marks.values():
+    print(val)
