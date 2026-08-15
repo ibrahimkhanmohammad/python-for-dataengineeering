33a32347c59dff445d12cc1ae1970428da34b470
ibrahimkhanmohammad <ibrahimfirdousmd@gmail.com>
2026-08-15 10:32:49 +0530

    dict comprehension using zip

--- 017_dictionaries/dictionary comprehension/dictionary comprehension using zip/zipping.py
+++ 017_dictionaries/dictionary comprehension/dictionary comprehension using zip/zipping.py
@@ -1,4 +1,4 @@
-# zip() pairs corresponding elements of two lists.
+# zip() is a built-in Python function that combines two or more iterables element-wise and returns a zip object containing tuples of corresponding elements
 # It returns a zip object, and each pair is a tuple.
 
 subjects = ['Math', 'Science', 'English']
