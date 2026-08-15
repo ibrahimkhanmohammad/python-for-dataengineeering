4b608d1f601c283505037d04ccd0193c93b7e4dc
ibrahimkhanmohammad <ibrahimfirdousmd@gmail.com>
2026-08-15 20:11:24 +0530

    basics on sets

---
+++ 018_sets/01_basic_sets/01_empty_set.py
@@ -0,0 +1,9 @@
+#   to create an empty set we should use set() which is also known as set constructor
+my_set = set()
+print(my_set)
+print(type(my_set))
+
+my_sett = {}
+print(my_sett)
+print(type(my_sett))
+
---
+++ 018_sets/01_basic_sets/02_print_set.py
@@ -0,0 +1,2 @@
+details = {'aizen', 999, 999.999, True}
+print(details)  #   the order may vary because it is an unordered data type
