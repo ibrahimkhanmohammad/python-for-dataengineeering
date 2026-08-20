b2368ba837e898019c39cdeb4584e937e452206b
ibrahimkhanmohammad <ibrahimfirdousmd@gmail.com>
2026-08-20 18:41:57 +0530

    methods of split and join in strings

---
+++ 019_strings/09_string methods (splitting and joining)/01 methods/01_split.py
@@ -0,0 +1,11 @@
+#   split() - split the string into a list
+
+sentence = 'python is crazy and fun'
+
+ans = (sentence.split())     # by-default it split based on space
+print(ans)
+print(type(ans))
+
+email = 'abc@gmail.com'
+mail = email.split('@')
+print(mail)
---
+++ 019_strings/09_string methods (splitting and joining)/01 methods/02_join.py
@@ -0,0 +1,12 @@
+#   join() - join the list into str
+
+words = ['python', 'is', 'fun']
+
+sentence = ' '.join(words)
+print(sentence)
+
+result = '-'.join(words)
+print(result)
+
+items = ['bread','eggs','milk','curd']
+print('Shopping list: ' + ', '.join(items))
